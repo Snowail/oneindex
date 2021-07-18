61d87602045230f5e83740ad22cf240f58a2e8d7
Snowail <45956036+Snowail@users.noreply.github.com>
2021-07-19 00:22:38 +0800

    update predis

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '78e76b354c3d949a311a05237f04ecfb9183ef03',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '78e76b354c3d949a311a05237f04ecfb9183ef03',
+            'dev_requirement' => false,
+        ),
+        'predis/predis' => array(
+            'pretty_version' => 'v1.0.3',
+            'version' => '1.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../predis/predis',
+            'aliases' => array(),
+            'reference' => '84060b9034d756b4d79641667d7f9efe1aeb8e04',
+            'dev_requirement' => false,
+        ),
+    ),
+);
